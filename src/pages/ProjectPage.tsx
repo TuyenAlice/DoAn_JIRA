29b0d724246b6bbee3336d595ead6e831bd39fde
orson <truonghung.dev@gmail.com>
2024-10-14 13:38:41 +0700

    feat: ProjectDetailComponent

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,59 +1,87 @@
 import { useEffect, useRef, useState } from "react";
 import { Button } from "antd";
-import { getAllProject, getProjectCategory } from "../services/project-service";
-import { ProjectCategory, ProjectList } from "../interfaces/ProjectInterface";
+import { getAllProject, getProjectCategory, getProjectDetail } from "../services/project-service";
+import { ProjectCategory, ProjectDetail, ProjectList } from "../interfaces/ProjectInterface";
 import JiaHeder from "../components/Header";
 import ProjectTable from "../components/ProjectTable";
+import ProjectDetai from "../components/ProjectDetail";
 
 const ProjectPage = () => {
-  const [projects, setProjects] = useState<ProjectList[]>([]);
-  const [projectCats, setProjectCats] = useState<ProjectCategory[]>();
-  const hasLoaded = useRef(false);
+    const [projects, setProjects] = useState<ProjectList[]>([]);
+    const [projectCats, setProjectCats] = useState<ProjectCategory[]>();
+    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+    const [isCreateMode, setIsCreateMode] = useState<boolean>(false);
+    const [selectedProject, setSelectedProject] = useState<ProjectDetail>();
+    const hasLoaded = useRef(false);
 
-  const onLoad = async () => {
-    const promiseGetProjectCats = async (): Promise<any> => {
-      const response = await getProjectCategory();
-      return response;
+    const onLoad = async () => {
+        const promiseGetProjectCats = async (): Promise<any> => {
+            const response = await getProjectCategory();
+            return response;
+        };
+
+        const promiseGetAllProject = async (): Promise<any> => {
+            const response = await getAllProject();
+            return response;
+        };
+
+        Promise.all([promiseGetProjectCats(), promiseGetAllProject()]).then(
+            ([resProjectCats, resProjects]: any) => {
+                setProjectCats(resProjectCats.data);
+                setProjects(resProjects.data);
+                console.log(resProjectCats.data, resProjects.data);
+            }
+        );
     };
 
-    const promiseGetAllProject = async (): Promise<any> => {
-      const response = await getAllProject();
-      return response;
+    const onDetail = async (id: number) => {
+        await getProjectDetail(id).then((response) => {
+            setSelectedProject(response.data);
+            setIsModalOpen(true);
+            setIsCreateMode(false);
+        }).catch((error) => {
+            alert(error.response.data.message);
+        });
     };
 
-    Promise.all([promiseGetProjectCats(), promiseGetAllProject()]).then(
-      ([resProjectCats, resProjects]: any) => {
-        setProjectCats(resProjectCats.data);
-        setProjects(resProjects.data);
-        console.log(resProjectCats.data, resProjects.data);
-      }
-    );
-  };
+    const onUpdate = (data: ProjectDetail) => {
+        setIsModalOpen(false);
+        setSelectedProject(undefined);
+        console.log("updated", data);
+    };
 
-  const onDetail = (id: number) => {};
+    const onDelete = (id: number) => { };
 
-  const onEdit = (id: number) => {};
-
-  const onDelete = (id: number) => {};
-
-  useEffect(() => {
-    if (!hasLoaded.current) {
-      onLoad();
-      hasLoaded.current = true;
+    const closeModal = () => {
+        setIsModalOpen(false);
+        setSelectedProject(undefined);
     }
-  }, [projects, projectCats]);
-  return (
-    <div>
-      <h1>Projects</h1>
-      <Button type="primary">Add Project</Button>
-      <ProjectTable
-        data={projects}
-        onDetail={onDetail}
-        onEdit={onEdit}
-        onDelete={onDelete}
-      />
-    </div>
-  );
+
+    useEffect(() => {
+        if (!hasLoaded.current) {
+            onLoad();
+            hasLoaded.current = true;
+        }
+    }, [projects, projectCats]);
+    return (
+        <div>
+            <h1>Projects</h1>
+            <Button type="primary">Add Project</Button>
+            <ProjectTable
+                data={projects}
+                onDetail={onDetail}
+                onEdit={onDetail}
+                onDelete={onDelete}
+            />
+            <ProjectDetai
+                data={selectedProject}
+                categories={projectCats}
+                isCreate={isCreateMode}
+                isOpen={isModalOpen}
+                onClose={closeModal}
+                onSubmit={onUpdate} />
+        </div>
+    );
 };
 
 export default ProjectPage;
